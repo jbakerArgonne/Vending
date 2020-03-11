ad024f579dec1dcb4504df4c021d41b74033103e
jbaker <jpbaker@anl.gov>
2020-03-11 13:44:56 -0500

    Add project files.

---
+++ Vending.Tests/TemperatureConverterTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Kaizenko.TempConv.Tests
+{
+    public class TemperatureConverterTests
+    {
+        [Test]
+        public void ConvertCtoF_When0C_Expect32F()
+        {
+           //ARRANGE
+            TemperatureConverter tempConverter = new TemperatureConverter();
+           //ACT
+            double tempInF =  tempConverter.ConvertCtoF(0);
+            //ASSERT
+         
+            Assert.AreEqual(32, tempInF);
+        }
+
+        [Test]
+        public void ConvertCtoF_When100C_Expect212F()
+        {
+           TemperatureConverter tempConverter = new TemperatureConverter();
+           double tempInF = tempConverter.ConvertCtoF(100);
+           Assert.AreEqual(212, tempInF);
+          //Assert.That(tempInF, isW)
+        }
+
+        [Test]
+        public void ConvertCtoF_WhenNegative40_ExpectNegative40F()
+        {
+           TemperatureConverter tempConverter = new TemperatureConverter();
+           double tempInF = tempConverter.ConvertCtoF(-40);
+            Assert.AreEqual(-40, tempInF);
+        }
+
+        [Test]
+        public void ConvertCtoF_When37C_Expect98Point6()
+        {
+            TemperatureConverter tempConverter = new TemperatureConverter();
+            double tempInF = tempConverter.ConvertCtoF(37);
+            Assert.AreEqual(98.6, tempInF);
+        }
+
+    }
+
+}
---
+++ Vending.Tests/VendingMachineTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Vending.Tests
+{
+    public class VendingMachineTest
+    {
+        [Test]
+        public void test1()
+        {
+            //ARRANGE
+           VendingMachine vending_machine = new VendingMachine();
+          
+            //ACT
+
+            //ASSERT
+            Assert.Fail();
+           
+        }
+
+    }
+}
